1b845df331166dc59af3cbc88d6a0c70c6ba01a1
Bryant Richards <dragonseyeimages@gmail.com>
2019-12-06 18:41:13 -0600

    Created article MVP and CRUD

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,56 @@
 class EventsController < ApplicationController
+
+  def index
+    if @newsletter = Newsletter.find_by_id(params[:newsletter_id])
+        @events = @newsletter.events.all
+    else
+        @events = Event.all
+    end
+  end
+
+  def new
+    if @newsletter = Newsletter.find_by_id(params[:newsletter_id])
+        @event = @newsletter.events.build
+    else
+        @event = Event.new
+    end
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.byline = current_user.last_name
+
+    if @event.save
+      redirect_to @event
+    else
+      render :new
+    end
+  end
+
+  def show
+    @event = Event.find_by_id(params[:id])
+  end
+
+  def edit
+    @event = Event.find_by_id(params[:id])
+  end
+
+  def update
+    @event = Event.find_by_id(params[:id])
+    @event.update(event_params)
+    redirect_to event_path(@event)
+  end
+
+  def destroy
+    @event = Event.find_by_id(params[:id])
+    @event.destroy
+    redirect_to events_path
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:title, :byline, :body, :newsletter_id, :photo)
+  end
+end
+
 end
