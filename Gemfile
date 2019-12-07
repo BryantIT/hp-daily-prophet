d1d239c293e3931da5d33d084500a78d985db571
Bryant Richards <dragonseyeimages@gmail.com>
2019-12-06 21:43:59 -0600

    Seeded some data, add sepia effect to photos

--- Gemfile
+++ Gemfile
@@ -23,9 +23,9 @@ gem 'jbuilder', '~> 2.7'
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
+gem "mini_magick"
 # Use Active Storage variant
-# gem 'image_processing', '~> 1.2'
+gem 'image_processing', '~> 1.2'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
@@ -36,6 +36,7 @@ gem 'omniauth'
 gem 'omniauth-facebook'
 # https://github.com/zquestz/omniauth-google-oauth2
 gem 'omniauth-google-oauth2'
+gem 'faker'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# 10.times do
+#     User.create(
+#       first_name: Faker::Name.name,
+#       last_name: Faker::Name.name,
+#       email: Faker::Internet.email,
+#       password: Faker::Internet.password
+#     )
+# end
+
+# 10.times do
+#     Newsletter.create(
+#       title: Faker::Book.title,
+#       sub_title: Faker::Lorem.sentence(word_count: 6),
+#       publish_date: Faker::Date.backward(days: 14),
+#       summary: Faker::Lorem.paragraph(sentence_count: 4),
+#       user_id: rand(20..25)
+#     )
+# end
+
+# 10.times do
+#     Article.create(
+#       title: Faker::Book.title,
+#       byline: Faker::Name.name,
+#       body: Faker::Lorem.paragraph(sentence_count: 4),
+#       newsletter_id: rand(10..17),
+#     )
+# end
