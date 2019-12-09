53cd884bf67ef4a9c943c1e1d57ad9b4cf8ec745
Bryant Richards <dragonseyeimages@gmail.com>
2019-12-08 19:49:45 -0600

    Basic facebook auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :github, :google_oauth2]
 
 
 has_one_attached :avatar
@@ -10,12 +10,12 @@ has_many :newsletters
 has_many :articles, through: :newsletters
 has_many :events, through: :articles
 
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
+def self.create_from_provider_data(provider_data)
+    where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do | user |
+      user.first_name = provider_data.info.first_name
+      user.last_name = provider_data.info.last_name
+      user.email = provider_data.info.email
+      user.password = Devise.friendly_token[0, 20]
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :articles
   resources :newsletters
   root to: 'application#welcome'
-  devise_for :users, path: 'users', :controllers => {registrations: 'registrations', omniauth_callbacks: 'callbacks'},
+  devise_for :users, path: 'users', :controllers => {registrations: 'registrations', omniauth_callbacks: 'omniauth'},
   :path_names => {
     :sign_in => 'login',
     :sign_out => 'logout',
