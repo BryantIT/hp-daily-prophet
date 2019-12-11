579e093e97465d89c5f6a66eb5be0b2f0f2aab5d
Bryant Richards <dragonseyeimages@gmail.com>
2019-12-10 18:53:35 -0600

    deleted unused code

--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -10,21 +10,4 @@ class Newsletter < ApplicationRecord
 
   scope :my_newsletters, -> (id) {where(user_id: id) }
 
-  def archive
-    # move all newsletters older than one year to archive table
-  end
-
-  def sort_by_year
-    # sort by year
-  end
-
-  def sort_by_month
-    # sort by month
-  end
-
-  def sort_by_author
-    # sorth by author
-  end
-
-
 end
